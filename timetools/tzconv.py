3d04e97289be37640564d00706e55a613e77e3da
Ross Jennings <rossjjennings@gmail.com>
2024-02-07 15:54:12 -0500

    Separate newzone argument

--- timetools/tzconv.py
+++ timetools/tzconv.py
@@ -5,11 +5,12 @@ def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action='store_true', help="display additional information about time zone on output")
-    parser.add_argument("time", nargs='+', help="time (YYYY-MM-DD [HH:MM:SS[.f]] [TZ])")
+    parser.add_argument("time", nargs='+', help="time (YYYY-MM-DD [HH:MM:SS[.f]] TZ)")
+    parser.add_argument("newzone", help="new time zone (IANA tz database key)")
     args = parser.parse_args()
 
-    time = datetime_from_args(args.time[:-1])
-    newzone = ZoneInfo(args.time[-1])
+    time = datetime_from_args(args.time)
+    newzone = ZoneInfo(args.newzone)
     time = time.astimezone(newzone)
     if args.verbose:
         print(f"{time.strftime('%Y-%m-%d %H:%M:%S %Z%z')} [{time.tzinfo}]")
