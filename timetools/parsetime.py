4d6c144e4c16bf5ec234178b96ca2ffd615423ea
Ross Jennings <rossjjennings@gmail.com>
2024-02-07 22:29:08 -0500

    Fix 'now' special case

--- timetools/parsetime.py
+++ timetools/parsetime.py
@@ -7,7 +7,7 @@ def datetime_from_args(args, default_tz=None):
         tz = ZoneInfo(args[-1])
         args.pop()
     except ZoneInfoNotFoundError:
-        if '-' in args[-1] or ':' in args[-1]:
+        if '-' in args[-1] or ':' in args[-1] or args[-1] == 'now':
             # no time zone was specified, assume default
             tz = get_localzone() if default_tz is None else default_tz
         else:
